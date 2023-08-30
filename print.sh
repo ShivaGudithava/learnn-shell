9edcff048225cd21f80d678e537ee05be89bbd92
LAPTOP-7BJSCJKS\ashok <ashok.csecr@gmail.com>
2023-08-30 14:10:50 +0530

    print coloures

--- print.sh
+++ print.sh
@@ -3,4 +3,16 @@ echp hello world
 # red      31
 # greeen   32
 # yellow   33
-# blue     36
+# blue     34
+# magenta  35
+# cyan     36
+
+# syntax
+# echo -e "\e[CODEmMESSAGE\e[0m"
+
+echo -e "\e[31mHello World\e[0m"
+echo -e "\e[32mHello World\e[0m"
+echo -e "\e[33mHello World\e[0m"
+echo -e "\e[34mHello World\e[0m"
+echo -e "\e[35mHello World\e[0m"
+echo -e "\e[36mHello World\e[0m"
