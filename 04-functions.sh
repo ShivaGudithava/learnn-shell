6e05ac300404197f51e34a888ddbae10934ed480
LAPTOP-7BJSCJKS\ashok <ashok.csecr@gmail.com>
2023-09-09 14:37:16 +0530

    schema setup

--- 04-functions.sh
+++ 04-functions.sh
@@ -1,8 +1,21 @@
 greeting() {
    echo Hello, Good Morning
    echo Welcome to Devops Training
+   return 10
    echo Good to Have you hear.
 }
 
 
 greeting
+echo function  Exit Status - $?
+
+# You declare var in main program, you can access that in function and vice-versa
+# Function have its own special variables
+
+ input() {
+   echo First Arugument - $1
+   echo Second Arugument - $2
+   echo ALL Arugument - $*
+   echo No of Arugument - $#
+}
+ input abc 1234
