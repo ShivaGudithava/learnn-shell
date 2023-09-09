632d9e6553404059bdbc495c10c288d5d383907f
LAPTOP-7BJSCJKS\ashok <ashok.csecr@gmail.com>
2023-09-09 14:15:11 +0530

    schema setup

--- 03-var.sh
+++ 03-var.sh
@@ -1,2 +1,16 @@
+# Varibles
 a=10
 echo a is $a
+
+# Special Variables
+# $0 - $N, $*, $#
+
+# Substitution Variables
+## Command Substitution
+Date=$(date)
+
+echo Today Date Is $Date
+
+## Arthmetic Substitution
+ADD =$(( 2+2))
+echo ADD of 2+2 = $ADD
